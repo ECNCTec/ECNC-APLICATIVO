12d1c59f9bf77cd4c2e2f2d4726d78ebcd84d9d6
ECNCTec <tecnologiaecnc@gmail.com>
2025-02-07 17:19:57 -0300

    fix: adiciona correcao no controller estoque para trazer somente informacoes do usuario logado

--- app/Http/Controllers/EstoqueController.php
+++ app/Http/Controllers/EstoqueController.php
@@ -5,13 +5,15 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use App\Models\Produto;
 use App\Models\Fornecedor;
+use Illuminate\Support\Facades\Auth;
 
 class EstoqueController extends Controller
 {
     public function informacoesDoSistema()
     {
-        $produtos = Produto::all();
-        $fornecedores = Fornecedor::all();
+        $produtos = Produto::where('user_id', Auth::id())->get();
+        $fornecedores = Fornecedor::where('user_id', Auth::id())->get();
+        
         return view('cadastroEstoque', compact('produtos', 'fornecedores'));
     }
 }
