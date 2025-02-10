136e16359e1fdf98b3ccbd08a22efb3adb80e429
ECNCTec <tecnologiaecnc@gmail.com>
2025-02-10 10:03:43 -0300

    feat: adiciona o css e script para as mensagens de aviso na tela de clientes

--- resources/views/cadastroClientes.blade.php
+++ resources/views/cadastroClientes.blade.php
@@ -46,6 +46,30 @@
                 font-size: 12px;
             }
 
+            .message {
+                position: absolute;
+                top: 50%;
+                left: 50%;
+                transform: translate(-50%, -50%);
+                padding: 20px;
+                border-radius: 1px;
+                z-index: 1000;
+                min-width: 200px;
+                text-align: center;
+                font-size: 16px;
+                opacity: 1;
+                transition: opacity 1s ease-out;
+            }
+
+            .alert-success,
+            .alert-warning,
+            .alert-danger {
+                background: linear-gradient(10deg, #c8d8f1d3, #dee9f9d3, #c8d8f1d3, #dee9f9d3);
+                color: rgb(40, 40, 40);
+                font-weight: bold;
+                border: none;
+            }
+
             .button-cadastrar {
                 background: linear-gradient(to bottom, #3387fb, #4242ff, #3387fb);
                 color: #fff;
@@ -202,6 +226,21 @@
 
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/js/bootstrap.bundle.min.js"></script>
+    {{-- script para controle do tempo de aparição das mensagens no sistema --}}
+    <script>
+        document.addEventListener("DOMContentLoaded", function() {
+            const messages = document.querySelectorAll('.message');
+            messages.forEach(function(message) {
+                setTimeout(function() {
+                    message.style.opacity = '0';
+                }, 2000);
+
+                setTimeout(function() {
+                    message.remove();
+                }, 15000);
+            });
+        });
+    </script>
 </body>
 
 </html>
