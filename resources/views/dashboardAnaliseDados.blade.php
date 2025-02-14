8e06207e257423e5abe147f73b79ff85cd559103
ECNCTec <tecnologiaecnc@gmail.com>
2025-02-14 08:14:43 -0300

    feat: adiciona graficos nos cards

--- resources/views/dashboardAnaliseDados.blade.php
+++ resources/views/dashboardAnaliseDados.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <title>Clientes</title>
     </style>
 </head>
@@ -105,7 +106,7 @@
                                 <h6 class="porcentagem ml-auto mr-1">48%</h6>
                             </div>
                             <div>
-                                <h6>gráfico aqui</h6>
+                                <canvas id="graficoReceita" height="40"></canvas>
                             </div>
                         </div>
                     </div>
@@ -116,6 +117,9 @@
                                 <h6 class="valor ml-1"><strong>R$</strong> 2.390,00</h6>
                                 <h6 class="porcentagem ml-auto mr-1">21%</h6>
                             </div>
+                            <div>
+                                <canvas id="graficoAprovados" height="40"></canvas>
+                            </div>
                         </div>
                     </div>
                     <div class="col-lg-3 col-md-6 col-sm-12">
@@ -124,6 +128,9 @@
                             <div class="valoresCards">
                                 <h6 class="valor ml-1"><strong>R$</strong> 2.390,00</h6>
                                 <h6 class="porcentagem ml-auto mr-1">5%</h6>
+                            </div>
+                            <div>
+                                <canvas id="graficoPendentes" height="40"></canvas>
                             </div>
                         </div>
                     </div>
@@ -134,11 +141,61 @@
                                 <h6 class="valor ml-1"><strong>R$</strong> 2.390,00</h6>
                                 <h6 class="porcentagem ml-auto mr-1">35%</h6>
                             </div>
+                            <div>
+                                <canvas id="graficoEstoque" height="40"></canvas>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+        <script>
+            function criarGrafico(id, dataValores, cor) {
+                var ctx = document.getElementById(id).getContext('2d');
+                new Chart(ctx, {
+                    type: 'line',
+                    data: {
+                        labels: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
+                        datasets: [{
+                            label: id,
+                            data: dataValores,
+                            borderColor: cor,
+                            backgroundColor: cor.replace('1)', '0.1)'), 
+                            borderWidth: 1,
+                            fill: true,
+                            tension: 0.48,
+                            pointRadius: 0,
+                            pointHoverRadius: 4
+                        }]
+                    },
+                    options: {
+                        responsive: true,
+                        maintainAspectRatio: false,
+                        scales: {
+                            x: {
+                                display: false
+                            },
+                            y: {
+                                display: false
+                            }
+                        },
+                        plugins: {
+                            legend: {
+                                display: false
+                            }
+                        }
+                    }
+                });
+            }
+            criarGrafico('graficoReceita', [1100, 1000, 1500, 1200, 1800, 2390, 1500, 1200, 2800, 2390, 1500, 1200],
+                'rgba(0, 86, 179, 1)');
+            criarGrafico('graficoAprovados', [400, 1900, 1400, 1100, 1600, 2200, 1300, 1100, 2600, 2200, 1400, 1100],
+                'rgba(0, 128, 96, 1)');
+            criarGrafico('graficoPendentes', [3000, 700, 1200, 900, 1500, 1200, 2200, 1000, 2400, 2000, 1300, 1700],
+                'rgba(204, 163, 0, 1)');
+            criarGrafico('graficoEstoque', [1200, 600, 1100, 800, 1400, 1900, 1100, 900, 2300, 1900, 1200, 1900],
+                'rgba(153, 40, 40, 1)');
+        </script>
     @endsection
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/js/bootstrap.bundle.min.js"></script>
